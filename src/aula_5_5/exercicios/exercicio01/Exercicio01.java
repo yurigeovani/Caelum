c3085c18768bdd83d0ad05c876df88813e794dcd
Yuri Geovani <yuri_geovani@hotmail.com>
2019-02-06 21:58:50 -0300

    Aula 5.5

---
+++ src/aula_5_5/exercicios/exercicio01/Exercicio01.java
@@ -0,0 +1,18 @@
+package aula_5_5.exercicios.exercicio01;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+
+//		1) Volte ao nosso sistema de Funcionario e crie uma classe Empresa dentro do mesmo arquivo .java. A
+//			Empresa tem um nome, cnpj e uma referência a uma array de Funcionario, além de outros atributos que
+//				você julgar necessário.
+		
+//			class Empresa {
+//			// outros atributos
+//			Funcionario[] empregados;
+//			String cnpj;
+//			}
+		
+	}
+}
